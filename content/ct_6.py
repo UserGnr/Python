4ca01a90e4814eac51ff742d80d0249a6b0247e9
André Matos Decaris <generaldatauser@gmail.com>
2024-07-06 23:22:54 -0300

    Adicionando o tipo None no arquivo ct_6

--- content/ct_6.py
+++ content/ct_6.py
@@ -5,6 +5,8 @@ False   Falso
 Obs: em Python é errado que o True e False fiquem com as letras minúsculas 
 '''
 
+print('Booleano')
+
 # Fazer testes nos valores e nos operadores abaixo
 ativo = False
 
@@ -15,3 +17,27 @@ else:
 
 print(ativo)
 print(not ativo)
+
+print('\n\n')
+
+'''
+None
+
+O tipo de dado None em Python representa o tipo sem tipo, poderia ser conhecido como tipo vazio, porém dizer que é um tipo sem tipo é mais apropriado
+
+O tipo None é SEMPRE com o 'N' em maiúsculo e as demais em minúsculo
+
+None pode ser utilizado para inicializar uma variável, antes de receber um valor final 
+'''
+
+print('None')
+
+numeros = None
+
+print(f'Valor da variável numeros: {numeros}')
+print(f'Tipo da variável numeros: {type(numeros)}')
+
+numeros = 1.44, 1.34, 5.67
+
+print(f'Valor da variável numeros: {numeros}')
+print(f'Tipo da variável numeros: {type(numeros)}')

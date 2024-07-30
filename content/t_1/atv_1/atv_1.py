52717fdb6275c9775c9e1447afbb5573b99d5b65
André Matos Decaris <generaldatauser@gmail.com>
2024-07-30 08:15:18 -0300

    Adicionando o arquivo 'ct_6' na pasta 't_14' sobre herança

--- content/t_1/atv_1/atv_1.py
+++
@@ -1,5 +0,0 @@
-# 1 - Faça um programa que leia um número inteiro e imprima-o.
-
-numero: int = int(input('Digite um número inteiro: '))
-
-print(f'O número digitado é {numero}')
--- content/t_1/atv_1/atv_2.py
+++
@@ -1,7 +0,0 @@
-# 2 - Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
-
-texto = 'Digite um número inteiro: '
-numero1, numero2, numero3 = int(input(texto)), int(input(texto)), int(input(texto))
-soma: int = numero1 + numero2 + numero3
-
-print(f'A soma dos números digitados {numero1,numero2,numero3} é {soma}')

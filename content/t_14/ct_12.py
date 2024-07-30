05857a36ae95ca839f81e8d6e1ca214e1d3670eb
André Matos Decaris <generaldatauser@gmail.com>
2024-07-30 08:52:02 -0300

    Adicionando os arquivos 'ct_11' e 'ct_12' na pasta 't_14' sobre poliformismo e métodos mágicos

---
+++ content/t_14/ct_12.py
@@ -0,0 +1,58 @@
+'''
+Métodos mágicos
+
+- São todos os métodos que utilizam dunder
+- Dunder (Double Underscore)
+
+# dunder init -> __init__()
+def __init__(self, titulo, autor, paginas):
+    self.titulo = titulo
+    self.autor = autor
+    self.paginas = paginas
+
+# dunder repr -> Representação do objeto
+def __repr__(self):
+    return f'{self.titulo} escrito por {self.autor}'
+'''
+
+
+class Livro:
+
+    def __init__(self, titulo, autor, paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return self.titulo
+
+    def __repr__(self):
+        return f'{self.titulo} escrito por {self.autor}'
+
+    def __len__(self):
+        return self.paginas
+
+    def __del__(self):
+        print('Um objeto do tipo Livro foi deletado da memória')
+
+    def __add__(self, outro):
+        return f'{self} - {outro}'
+
+    def __mul__(self, outro):
+        if isinstance(outro, int):
+            msg = ''
+            for n in range(outro):
+                msg += ' ' + str(self)
+            return msg
+        return 'Não posso multiplicar'
+
+
+livro1 = Livro('Python Rocks!', 'Texto de teste', 400)
+livro2 = Livro('Inteligência Artificial com Python', 'Texto de teste', 350)
+
+print(livro1)
+print(livro2)
+print(len(livro1))
+print(len(livro2))
+print(livro1 + livro2)
+print(livro1 * 5)

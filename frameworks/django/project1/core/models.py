e269f44ed13ffbafa4210d23bb6cc1805922aa87
André Matos Decaris <generaldatauser@gmail.com>
2024-10-01 15:16:41 -0300

    Dando continuidade no desenvolvendo da aplicação django 'frameworks\django\project1'

--- frameworks/django/project1/core/models.py
+++ frameworks/django/project1/core/models.py
@@ -6,7 +6,13 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', max_digits=9,decimal_places=2)
     estoque = models.IntegerField('Quantidade em Estoque')
 
+    def __str__(self):
+        return f'{self.nome}'
+
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField("Sobrenome", max_length=100)
     email = models.EmailField('E-mail', max_length=254)
+
+    def __str__(self):
+        return self.nome
--- frameworks/django/project1/pj1/settings.py
+++ frameworks/django/project1/pj1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-+^u6bjo#n*zw+6zid1^+g_j=p(ojxho9047n@*lg(2+88=a8gw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,9 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # Usado durante o desenvolvimento
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Usado durante a produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'   # Ao fazer logout da conta ADMIN, é redirecionado para a página index

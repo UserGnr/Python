b94253d6037f60909820d493a798dba5408300e6
André Matos Decaris <generaldatauser@gmail.com>
2024-10-04 12:26:50 -0300

    Trabalhando na parte dos formulários nos arquivos 'frameworks\django\project2'

--- frameworks/django/project2/core/views.py
+++ frameworks/django/project2/core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import ContatoForm
+
 
 # Create your views here.
 def index(request):
@@ -6,7 +10,29 @@ def index(request):
 
 
 def contato(request):
-    return(render(request,'contato.html'))
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'Assunto: {assunto}')
+            print(f'E-mail: {email}')
+            print(f'Mensagem: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
-    return render(request('produto.html'))
+    return render(request,'produto.html')

b657faf231637cf8820a11a6fc51e893d285e42e
André Matos Decaris <generaldatauser@gmail.com>
2024-10-09 15:50:42 -0300

    Desenvolvendo ações relacionadas ao produto em 'frameworks\django\my\project1'

--- frameworks/django/my/project1/core/models.py
+++ frameworks/django/my/project1/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Produto(Base):
+    nome = models.CharField('Nome', max_length=255)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=9)
+
+    def __str__(self):
+        return self.nome

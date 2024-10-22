1cb7f0c18c0a6ea56842c76665d9ae3e637b9740
André Matos Decaris <generaldatauser@gmail.com>
2024-10-22 11:14:21 -0300

    Adicionando os templates em 'frameworks\django\study\project3'

--- frameworks/django/study/project3/core/urls.py
+++ frameworks/django/study/project3/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from django import IndexView
+from .views import IndexView, E404View, E500View
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    # path("E404/", E404View.as_view(), name="E404"),
+    # path("E500/", E500View.as_view(), name="E500"),
+    # As novas versões do django não precisam criar as páginas de erro dessa forma, basta ter um template nomeado com o erro 404 e 500, por exemplo
 ]
--- frameworks/django/study/project3/core/views.py
+++ frameworks/django/study/project3/core/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class E404View(TemplateView):
+    template_name = '404.html'
+
+class E500View(TemplateView):
+    template_name = '500.html'

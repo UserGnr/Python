c63b2fe94fabfcfd0fc9fba4376310eff59e5f39
André Matos Decaris <generaldatauser@gmail.com>
2024-10-22 15:06:30 -0300

    Adicionando as views e vinculando os templates em 'frameworks\django\study\project3'

--- frameworks/django/study/project3/core/views.py
+++ frameworks/django/study/project3/core/views.py
@@ -1,10 +1,19 @@
 from django.views.generic import TemplateView
+from .models import Servico, Funcionario
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        # order_by('?') é para deixar a order aleatória
+        context['funcionarios'] = Funcionario.objects.all()
+        return context
 
 class E404View(TemplateView):
     template_name = '404.html'
 
 class E500View(TemplateView):
     template_name = '500.html'
+
